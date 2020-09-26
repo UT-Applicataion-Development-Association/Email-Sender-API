5577f5d907613253c65a846c0a06425830dd69e5
Zi Wang <zikevin.wang@mail.utoronto.ca>
2020-09-26 02:00:35 -0400

    add demo code

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
-from server.routes import user
+from server.routes import user, mail
 
 app = Flask(__name__)
 
+print("\033[93m" + "server/__init__.py " + "Initializing the server instance and registering blueprints" + "\033[0m")
 app.register_blueprint(user.userRoutes, url_prefix="/user")
+app.register_blueprint(mail.mailRoutes, url_prefix="/mail")
 
+print("\033[93m" + "=========== Try to visit localhost:8080/mail/ with HTTP GET and POST method =========== " + "\033[0m")
 app.run(host="127.0.0.1", port=8081, debug=True)
---
+++ server/services/mail.py
@@ -0,0 +1,3 @@
+def send(recipient, text):
+    print("\033[93m" + "server/services/mail.py" + "mail service called to send the mail" + "\033[0m")
+    print("Mail sent to "+recipient)
